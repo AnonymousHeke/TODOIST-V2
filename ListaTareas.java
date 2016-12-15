bab0d59797fef035073fe3da3ce2e98e3adf73ac
David García Labarga <davgarlab@gmail.com>
2016-12-15 12:20:21 +0100

    Añadir tarea

---
+++ ListaTareas.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class ListaTareas
+{
+    
+    private  ArrayList<Tarea> listaDeTareas;
+
+    public ListaTareas()
+    {
+        listaDeTareas = new ArrayList<Tarea>();        
+    }
+    
+}
---
+++ Tarea.java
@@ -0,0 +1,14 @@
+
+public class Tarea
+{
+
+    private String descripcion;
+    private boolean estadoTarea;
+
+    public Tarea(String nDescripcion)
+    {
+        descripcion = nDescripcion;
+        estadoTarea = false;
+    }
+
+}
